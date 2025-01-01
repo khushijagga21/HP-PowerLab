e5a8d260d441bf0dbe33caecee39645dade51dfd
khushijagga21 <khushijagga14@gmail.com>
2025-01-01 11:11:48 -0700

    Edited App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import LoginBox from './Page/Login';
@@ -6,7 +7,7 @@ import Signup from './Page/SignUp';
 import Home from './Page/Home';
 import Ordering from './Page/Order';
 import About from './Page/About';
-import Tips from './Page/Tips'
+import Tips from './Page/Tips';
 
 function App() {
   return (
@@ -18,8 +19,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/order" element={<Ordering />} />
         <Route path="/about" element={<About />} />
-        <Route path="/tips" element={<Tips />} />
-        
+        <Route path="/tips" element={<Tips />} /> 
+  
       </Routes>
       <Footer />
     </Router>
